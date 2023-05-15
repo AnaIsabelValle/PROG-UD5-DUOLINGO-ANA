2202c72959a40c45be9c11c0748e5f8b008bf1a7
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-15 16:17:33 +0100

    wip:(Diccionario) Modificándo datos

--- src/com/duolingo/diccionario/Diccionario.java
+++ src/com/duolingo/diccionario/Diccionario.java
@@ -14,47 +14,44 @@ public class Diccionario {
     }
 
     public boolean añadirpalabra(String palabra) {
-        String nuevaPalabra = nuevaPalabra(palabra);
-        if (palabra.length() == 0) {
+        String palabras_Sanitanizadas = palabra_Sanitanizada(palabra);
+        if (palabras_Sanitanizadas.length() == 0) {
             return false;
         }
 
-        String inicial;
-
-        inicial = nuevaPalabra.CharArt(0);
+        char inicial = palabras_Sanitanizadas.charAt(0);
         Set<String> palabras = diccionario.get(inicial);
         if (palabras == null) {
             palabras = new HashSet<String>();
             diccionario.put(inicial, palabras);
         }
-        return palabras.add(nuevaPalabra
-        );
+        return palabras.add(palabras_Sanitanizadas);
     }
 
     public boolean eliminarPalabra(String palabra) {
-        String eliminandoPalabra = eliminandoPalabra(palabra);
-        if (eliminandoPalabra.length() == 0) {
+        String palabras_Sanitanizadas = palabra_Sanitanizada(palabra);
+        if (palabras_Sanitanizadas.length() == 0) {
             return false;
         }
-        String inicial = eliminandoPalabra.CharArt(0);
+        char inicial = palabras_Sanitanizadas.charAt(0);
         Set<String> palabras = diccionario.get(inicial);
         if (palabras == null) {
             return false;
         }
-        return palabras.remove(eliminandoPalabra);
+        return palabras.remove(palabras_Sanitanizadas);
     }
 
     public boolean tenerPalabra(String palabra) {
-        String existePalabra = existePalabra(palabra);
-        if (existePalabra.length() == 0) {
+        String palabras_Sanitanizadas = palabra_Sanitanizada(palabra);
+        if (palabras_Sanitanizadas.length() == 0) {
             return false;
         }
-        String inicial = existePalabra.CharArt(0);
-        Set<String> palabras = diccionario.get(existePalabra);
+        char inicial = palabras_Sanitanizadas.charAt(0);
+        Set<String> palabras = diccionario.get(inicial);
         if (palabras == null) {
             return false;
         }
-        return palabras.contains(existePalabra);
+        return palabras.contains(palabras_Sanitanizadas);
     }
 
 
