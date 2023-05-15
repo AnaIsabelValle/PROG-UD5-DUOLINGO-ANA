70a5fce69a91880f3e85a1e38cedd8a1d1208b85
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-15 14:27:34 +0100

    wfeat(Diccionario) Creación médido añadrir palabra

--- src/com/duolingo/diccionario/Diccionario.java
+++ src/com/duolingo/diccionario/Diccionario.java
@@ -1,14 +1,35 @@
 package com.duolingo.diccionario;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Diccionario {
 
-    Map<String, Set<String>> diccionario;
+    private Map<String, Set<String>> diccionario;
 
     public Diccionario() {
         diccionario = new HashMap<String, Set<String>>();
     }
+
+    public boolean añadirpalabra(String palabra) {
+        String nuevaPalabra = nuevaPalabra(palabra);
+        if (palabra.length() == 0) {
+            return false;
+        }
+
+        String inicial;
+
+        inicial = nuevaPalabra.CharArt(0);
+        Set<String> palabras = diccionario.get(inicial);
+        if (palabras == null) {
+            palabras = new HashSet<String>();
+            diccionario.put(inicial, palabras);
+        }
+        return palabras.add(nuevaPalabra);
+    }
 }
+
+
+
